b05c2f010316b48246ea34b01c005d739f15b1fa
Asif  Hussain <136885484+asifhussain01@users.noreply.github.com>
2025-05-23 12:58:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find element at a given Index/find-element-at-a-given-index.cpp
@@ -0,0 +1,12 @@
+
+class Solution {
+  public:
+    int findElementAtIndex(int key, vector<int> &arr) {
+        // code here
+        for(int i=0; i<arr.size();i++){
+            if(i == key){
+                return arr[i];
+            }
+        }
+    }
+};
