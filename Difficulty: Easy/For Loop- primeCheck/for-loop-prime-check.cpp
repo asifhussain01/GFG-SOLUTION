301a086c9eede571a927d81a749db6ee8cdb6823
Asif  Hussain <136885484+asifhussain01@users.noreply.github.com>
2025-06-12 20:12:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/For Loop- primeCheck/for-loop-prime-check.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    string isPrime(int n) {
+
+        // Code here
+        
+
+       if (n <= 1)
+            return "No";
+
+        for (int i = 2; i <= sqrt(n); i++) {
+            if (n % i == 0)
+                return "No";
+        }
+
+        return "Yes";
+
+    }
+};
