38bbb05b5916bf56f229af9695f30bf97e6f403e
Asif  Hussain <136885484+asifhussain01@users.noreply.github.com>
2025-08-23 21:57:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
@@ -1,97 +1,40 @@
-//{ Driver Code Starts
-// Initial function template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-   
-   bool isPossible(vector<int>&arr, int k, int sol){
-       int pageSum =0;
-       int studentCount = 1;
-       for(int i = 0; i<arr.size(); i++){
-           if(arr[i] > sol){
-               return false;
-           }
-           if(pageSum + arr[i] > sol){
-               studentCount++;
-               pageSum = arr[i];
-               if(studentCount > k){
-                   return false;
-               }
-           }
-           else{
-               pageSum += arr[i];
-           }
-       }
-       return true;
-   }
-   
-    int findPages(vector<int> &arr, int k) {
-        // code here
-        
-        int start =0;
-        int end = accumulate(arr.begin(), arr.end(), 0);
-        int N = arr.size();
-        int ans =-1;
-        
-        if(N < k)return -1;
-        
-        while(start <= end){
-            int mid = start +(end - start)/2;
-            
-            if(isPossible(arr, k, mid)){
-                ans = mid;
-                end = mid -1;
-                
+    // Helper function: check if we can allocate with maxPages limit
+    bool isPossible(vector<int>& arr, int k, int maxPages) {
+        int students = 1;
+        int pages = 0;
+
+        for (int a : arr) {
+            if (a > maxPages) return false; // one book itself exceeds limit
+            if (pages + a > maxPages) {
+                students++;
+                pages = a;
+                if (students > k) return false;
+            } else {
+                pages += a;
             }
-            else{
-                start= mid+1;
+        }
+        return true;
+    }
+    
+    int findPages(vector<int> &arr, int k) {
+        int n = arr.size();
+        if (k > n) return -1; // cannot allocate
+
+        int low = *max_element(arr.begin(), arr.end());
+        int high = accumulate(arr.begin(), arr.end(), 0);
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (isPossible(arr, k, mid)) {
+                ans = mid;
+                high = mid - 1; // try smaller maximum
+            } else {
+                low = mid + 1; // need larger maximum
             }
         }
         return ans;
     }
-
-    
-    
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int test_case;
-    cin >> test_case;
-    cin.ignore();
-    while (test_case--) {
-
-        int d;
-        vector<int> arr, brr, crr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        getline(cin, input);
-        ss.clear();
-        ss.str(input);
-        while (ss >> number) {
-            crr.push_back(number);
-        }
-        d = crr[0];
-        int n = arr.size();
-        Solution ob;
-        int ans = ob.findPages(arr, d);
-        cout << ans << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
