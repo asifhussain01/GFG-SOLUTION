e7bc9a22e7bee01d2bf553cfdd33f1a5343ad956
Asif  Hussain <136885484+asifhussain01@users.noreply.github.com>
2025-09-07 19:02:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.cpp
+++ Difficulty: Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.cpp
@@ -1,106 +1,38 @@
-//{ Driver Code Starts
-// C++ program to merge k sorted arrays of size n each
-#include <bits/stdc++.h>
-using namespace std;
-
-// A Linked List node
-struct Node {
-    int data;
-    Node* next;
-
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};
-
-/* Function to print nodes in a given linked list */
-void printList(Node* node) {
-    while (node != NULL) {
-        printf("%d ", node->data);
-        node = node->next;
-    }
-    cout << endl;
-}
-
-
-// } Driver Code Ends
-/*Linked list Node structure
-
-struct Node
-{
-    int data;
-    Node* next;
-
-    Node(int x){
-        data = x;
-        next = NULL;
-    }
-
-};
-*/
-
 class Solution {
   public:
-    #define p pair<int,Node*>
+    struct compare {
+        bool operator()(Node* a, Node* b) {
+            return a->data > b->data; // min-heap by data
+        }
+    };
+
     Node* mergeKLists(vector<Node*>& arr) {
-        //CodeGenius
-        Node* newHead= new Node(-1);
-        priority_queue<p,vector<p>,greater<p>>pq;
-        for(int i=0;i<arr.size();i++){
-            Node*temp=arr[i];
-            pq.push({temp->data,temp});
+        priority_queue<Node*, vector<Node*>, compare> pq;
+
+        // Push the head of every linked list into the heap
+        for (auto head : arr) {
+            if (head != NULL) {
+                pq.push(head);
+            }
         }
-        Node *temp=newHead;
-        while(!pq.empty()){
-            Node* curr=pq.top().second;
+
+        // Dummy node to simplify result building
+        Node* dummy = new Node(0);
+        Node* tail = dummy;
+
+        // Extract the smallest element one by one
+        while (!pq.empty()) {
+            Node* node = pq.top();
             pq.pop();
-            temp->next=curr;
-            temp=temp->next;
-            if(curr->next) pq.push({curr->next->data,curr->next});
+
+            tail->next = node;
+            tail = tail->next;
+
+            if (node->next != NULL) {
+                pq.push(node->next);
+            }
         }
-        return newHead->next;
-        
+
+        return dummy->next;
     }
 };
-
-
-//{ Driver Code Starts.
-
-// Driver program to test the above functions
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int n;
-        cin >> n;
-        cin.ignore();
-
-        vector<Node*> v(n);
-
-        for (int i = 0; i < n; i++) {
-            string line;
-            getline(cin, line);
-            stringstream ss(line);
-
-            Node* head = new Node(0);
-            Node* temp = head;
-            int x;
-            while (ss >> x) {
-                Node* newNode = new Node(x);
-                temp->next = newNode;
-                temp = temp->next;
-            }
-            v[i] = head->next;
-        }
-
-        Solution ob;
-        Node* head = ob.mergeKLists(v);
-        printList(head);
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
